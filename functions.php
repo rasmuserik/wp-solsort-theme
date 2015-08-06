90c8b4d3b86b2a6fa99e72f82f9b05a784d02b60
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2015-08-06 09:12:27 +0200

    remove code for solsort-widget

--- functions.php
+++ functions.php
@@ -13,28 +13,4 @@ function script_solsortjs() {
 }
 
 add_action( 'wp_print_footer_scripts', 'script_solsortjs' );
-
-/**
- * Solsort Logo Widget
- */
-class solsort_logo extends WP_Widget {
-  /** constructor */
-  function __construct() {
-    parent::WP_Widget(false, $name = 'Solsort logo');  
-  }
-  /** @see WP_Widget::widget */
-  function widget($args, $instance) { 
-?>
-<aside style="text-align: center" class="widget">
-  <a href="https://solsort.com/">
-    <img src=/icons/solsort.png width=50%>
-    <h2 class="widget-title">solsort.com</h2>
-  </a>
-</aside>
-<?php
-  }
-}
-add_action('widgets_init', create_function('', 'return register_widget("solsort_logo");'));
-
-
 ?>
